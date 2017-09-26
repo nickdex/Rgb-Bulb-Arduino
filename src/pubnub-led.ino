b2bd82a7af0a454860d29c225efcf5a348b89c09
Nikhil Warke <nikwarke@gmail.com>
2017-09-26 23:21:57 +0530

    Refactor code to accept color values from pubnub

--- src/pubnub-led.ino
+++ src/pubnub-led.ino
@@ -1,4 +1,5 @@
 #include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
 #define PubNub_BASE_CLIENT WiFiClient
 //#define PUBNUB_DEBUG
 #define PUBNUB_DEFINE_STRSPN_AND_STRNCASECMP
@@ -7,23 +8,40 @@
 const char* ssid = "Web";
 const char* password = "simple#9";
 
-const int subLedPin = 2; // Blue
-const int pubLedPin = 4; // Green
+#define BLUE_PIN  5
+#define GREEN_PIN 4
+#define RED_PIN   2
 
 char pubkey[] = "pub-c-a7a7bf57-7428-4acf-9feb-95aa76439442";
 char subkey[] = "sub-c-28e8300e-7e94-11e7-a179-1e66dc778462";
 char channel[] = "pi-house";
+
+void flash() {
+  digitalWrite(RED_PIN, HIGH);
+  delay(100);
+  digitalWrite(RED_PIN, LOW);
+  delay(100);
+
+  digitalWrite(GREEN_PIN, HIGH);
+  delay(100);
+  digitalWrite(GREEN_PIN, LOW);
+  delay(100);
+
+  digitalWrite(BLUE_PIN, HIGH);
+  delay(100);
+  digitalWrite(BLUE_PIN, LOW);
+  delay(100);
+}
 
 void setup() {
   Serial.begin(9600);
   delay(10);
 
   // prepare LEDS
-  pinMode(subLedPin, OUTPUT);
-  digitalWrite(subLedPin, LOW);
-
-  pinMode(pubLedPin, OUTPUT);
-  digitalWrite(pubLedPin, LOW);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  flash();
 
   // Connect to WiFi network
   Serial.println();
@@ -44,36 +62,8 @@ void setup() {
   Serial.println("PubNub set up");
 }
 
-void flash(int ledPin) {
-  /* Flash LED three times. */
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(ledPin, HIGH);
-    delay(100);
-    digitalWrite(ledPin, LOW);
-    delay(100);
-  }
-}
-
-
-// Publishes A Message
-void publishMessage() {
-  WiFiClient *client;
-  Serial.println("publishing a message");
-  client = PubNub.publish(channel, "\"Hi from ESP\"");
-  if (!client) {
-    Serial.println("publishing error");
-    delay(1000);
-    return;
-  }
-  while(client->connected()) {
-    while(client->connected() && !client->available()); //wait
-    char c = client->read();
-    Serial.print(c);
-  }
-  client->stop();
-  Serial.println();
-  flash(pubLedPin);
-}
+const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + 50;
+StaticJsonBuffer<bufferSize> jsonBuffer;
 
 void subscribeMessage() {
   Serial.println("waiting for a message (subscribe)");
@@ -83,21 +73,41 @@ void subscribeMessage() {
     delay(1000);
     return;
   }
+  char json[57];
+  byte i = 0;
   while(pclient->wait_for_data()) {
     char c = pclient->read();
-    Serial.print(c);
+    if (!(c == '[' || c == ']')) {
+      json[i++] = c;
+    }
   }
+  json[i] = 0;
+  if (i == 0) return;
+
   pclient->stop();
   Serial.println();
-  flash(subLedPin);
+
+  Serial.println(json);
+  JsonObject& root = jsonBuffer.parseObject(json);
+  JsonObject& value = root["value"];
+  if (!value.success()) {
+      Serial.println("Parsing failed");
+      return;
+  }
+
+  byte red = value["red"];
+  byte green = value["green"];
+  byte blue = value["blue"];
+
+  Serial.println(red);
+  Serial.println(green);
+  Serial.println(blue);
 }
 
 void loop() {
 
-//  publishMessage();
-
   subscribeMessage();
 
-  delay(10000);
+  delay(1000);
 
 }
